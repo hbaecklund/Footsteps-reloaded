5bdbe437a5686ff2785cb35c9fcaa13dcf46d3f9
Henrik Baecklund SourceTree <henrikbaecklund@gmail.com>
2013-10-29 09:52:06 +0100

    CSS MAGIC // HENRIK

--- js/main.js
+++ js/main.js
@@ -8,8 +8,17 @@ var routeData = [];
 var routes = {};
 var selectedRoute = null;
 var routeLinesRef = new Firebase('https://projectfootsteps.firebaseio.com/');
+var routePaths = new Firebase('https://projectfootsteps.firebaseio.com/paths/');
 
 function initialize() {
+
+	var greymap = [
+	{
+		featureType: "all",
+		elementType: "all",
+		stylers: [ { saturation: -100 } ]
+	}];
+
 	var mapOptions = {
 		center: new google.maps.LatLng(59.3294, 18.0686),
 		zoom: 14,
@@ -18,32 +27,39 @@ function initialize() {
 	};
 	map = new google.maps.Map(document.getElementById("map"),mapOptions);
 	dirDisplay.setMap(map);
-	var heatmap = new google.maps.visualization.HeatmapLayer({
-		data: dataArray,
-		radius: 15,
-		dissipate: true,
-		maxIntensity: 10
-	});
-	heatmap.setMap(map);
+
+	var mapType = new google.maps.StyledMapType(greymap, { name:"Grayscale" });
+	map.mapTypes.set('thegrey', mapType);
+	map.setMapTypeId('thegrey');
+
+// var heatmap = new google.maps.visualization.HeatmapLayer({
+// data: dataArray,
+// radius: 15,
+// dissipate: true,
+// maxIntensity: 10
+// });
+// heatmap.setMap(map);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
-$('.txtLoc').keyup(function(){
-	if ($('#startLoc').val() != "" && $('#endLoc').val() != "")
-	{
-		$('#btnAddTrip2');
-	}
-	else
-	{
-		$('#btnAddTrip2');
-	}
-});
+// $('.form-group').keyup(function(){
+// 	if ($('#startLoc').val() != "" && $('#endLoc').val() != "")
+// 	{
+// 		$('#btnAddTrip2');
+// 	}
+// 	else
+// 	{
+// 		$('#btnAddTrip2');
+// 	}
+// });
 
 function clearMap()
 {
 	dirDisplay.setMap(null)
-	$('#btnSaveTrip');
+	//$('#btnSaveTrip');
 	$('#btnClearMap');
+	map.setCenter(new google.maps.LatLng(59.3294, 18.0686), 12);
 }
 
 function plotTrip()
@@ -63,18 +79,18 @@ function plotTrip()
 	});
 }
 
-function tripStats()
-{
-	total = 0; 
-	var myroute = dirDisplay.directions.routes[0]; 
-	for (i = 0; i < myroute.legs.length; i++) { 
-		total += myroute.legs[i].distance.value; 
-	}
-	total = Math.round(total/100)/10;
-	startAdd = dirDisplay.directions.routes[0].legs[0].start_address; 
-	endAdd = dirDisplay.directions.routes[dirDisplay.directions.routes.length-1].legs[dirDisplay.directions.routes[dirDisplay.directions.routes.length-1].legs.length-1].end_address; 
-	$('#tripSpan').html('<b>Start Address:  </b>' + startAdd + ' | ' + '<b>End Address:  </b>' + endAdd + ' | ' + '<b>Distance:  </b> ' + total + "km")
-}
+// function tripStats()
+// {
+// 	total = 0; 
+// 	var myroute = dirDisplay.directions.routes[0]; 
+// 	for (i = 0; i < myroute.legs.length; i++) { 
+// 	total += myroute.legs[i].distance.value; 
+// 	}
+// 	total = Math.round(total/100)/10;
+// 	startAdd = dirDisplay.directions.routes[0].legs[0].start_address; 
+// 	endAdd = dirDisplay.directions.routes[dirDisplay.directions.routes.length-1].legs[dirDisplay.directions.routes[dirDisplay.directions.routes.length-1].legs.length-1].end_address; 
+// 	$('#tripSpan').html('<b>Start Address:  </b>' + startAdd + ' | ' + '<b>End Address:  </b>' + endAdd + ' | ' + '<b>Distance:  </b> ' + total + "km")
+// }
 
 function saveTrip()
 {
@@ -84,26 +100,32 @@ function saveTrip()
 			routeData.push(dirDisplay.directions.routes[0].legs[i].steps[j].path);
 		}
 	}
-	console.log(routeData);
-
-	routeLinesRef.push(routeData, function(error){
-		if (error) {
-			$('#savedSpan').html('Data could not be saved.' + error);
-		} else {
-			$('#savedSpan').html('Data saved successfully!');
-		}
-	});
+	var routeInput = document.getElementById('routeName');
+	var routeInput2 = routeInput.value;
+	var rrr = {name: routeInput2, paths: routeData};
+	routeLinesRef.push(rrr);
+	
+	// routeLinesRef.push(rrr, function(error){
+	// 	if (error) {
+	// 		$('#savedSpan').html('Data could not be saved.' + error);
+	// 	} else {
+	// 		$('#savedSpan').html('Data saved successfully!');
+	// 	}
+	// }
 }
 
 Array.prototype.clear = function() {
 	this.splice(0, this.length);
 };
 
+//DANIEL'S CODE STARTS HERE:::
+
 routeLinesRef.limit(10).on('child_added', function(snapshot)
 {
 	// loop over each route we get from firebase
-	route = snapshot.val();
+	route = snapshot.val().paths;
 	id = snapshot.name();
+
 	// make an array that is initially blank
 	// It will contain all the latitude and longitudes of each point on in the route
 	var routeCoordinates = [];
@@ -131,29 +153,32 @@ routeLinesRef.limit(10).on('child_added', function(snapshot)
 				}
 			}
 
-	console.log(routeCoordinates)
+	//console.log(routeCoordinates)
+
 	// Create a route on google maps.
 	var routePath = new google.maps.Polyline({
     	path: routeCoordinates, ///// THIS LINE HERE TELLS GOOGLE MAPS WHERE THE COORDINATES ARE FOR EACH POINT
     	geodesic: true,
-    	strokeColor: '#00FF7F', // if you want to change the color, do it here.
+    	strokeColor: '#FF0000', // if you want to change the color, do it here.
     	strokeOpacity: 0.5,
-    	strokeWeight: 5
+    	strokeWeight: 4
     });
+
   	// I have an object of routes. 
   	routes[id] = routePath;
+  	
   	// Add the route to the map
   	routePath.setMap(map);
-
+	
 	// Set the route to be invisible. By default, all routes are HIDDEN until you hover over them in the sidebar menu.
 	routePath.setVisible(false);
 
 	// Add the routes to the list of routes
-	// IN HERE YOU MUST ADD ANY ADDITIONAL NAMING THAT YOU WANT. EX: THE NAME OF THE ROUTE, ETC>>>>
-	$("#routesList").append("<li data-id=" + id + "><a href='#'>" + id + "</a></li>");
+	// IN HERE YOU MUST ADD ANY ADDITIONAL NAMING THAT YOU WANT. EX: THE NAME OF THE ROUTE, ETC!!!!!
+	$("#routesList").append("<li data-id=" + id + "><a href='#'>" + snapshot.val().name + "</a></li>");
 });
 
-$(function() {
+	$(function() {
 	var $menu = $('nav#menu'),
 	$html = $('html');
 
@@ -175,20 +200,20 @@ $(function() {
 
 		// Get the route that we selected. This comes from the data-id attribute that you can see on each <li>
 		selectedRoute = $(this).parent("li").data('id');
+		map.setZoom(14);
 
 		// Using the id that we found, we can look up the appropriate route in our "routes" object.
 		// Remember that we store an object with all the routes in it.
 		// ex: 
 		// routes = {
-		//     JQwrjwrwkjwler: ....,
-		//     JKwenrwwldfdsf: .....,	
+		// 	JQwrjwrwkjwler: ....,
+		//  JKwenrwwldfdsf: .....,	
 		// }
 		// Once we have the appropriate route, we set its visibility to true...effectively showing it. yay!
 		routes[selectedRoute].setVisible(true);
 
 		var center = new google.maps.LatLng(routes[selectedRoute].latLngs.b[0].b[0].lb, routes[selectedRoute].latLngs.b[0].b[0].mb)
 		map.setCenter(center);
-		map.setZoom(14);
 
 		// Closes the menu.
 		$menu.trigger("close");
@@ -203,7 +228,7 @@ $(function() {
 		routes[id].setVisible(true);
 		var center = new google.maps.LatLng(routes[id].latLngs.b[0].b[0].lb, routes[id].latLngs.b[0].b[0].mb)
 		map.setCenter(center);
-		map.setZoom(13);
+		map.setZoom(12);
 	}
 	);
 
@@ -234,7 +259,7 @@ $(function() {
 // 				{
 // 					dataArray.push(new google.maps.LatLng
 // 						(snapshot.val()[route][i][j].lb, snapshot.val()[route][i][j].mb));
-// 								//console.log(j + ' ' + snapshot.val()[route][i][j].lb, snapshot.val()[route][i][j].mb);
+// 								//console.log(j + ' ' + snapshot.val()[route][i][j].lb, snapshot.val()[route][i][j].mb);								
 // 							}
 // 						}
 // 					}
